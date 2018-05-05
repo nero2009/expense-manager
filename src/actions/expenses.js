e4347ec386175608c02611978fdc5016b848f3c5
nero <nero2009@rocketmail.com>
2018-05-05 20:22:47 +0100

    Setup fetch expenses from database

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -36,3 +36,25 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+//set_expenses
+export const setExpenses =(expenses)=>({
+  type:'SET_EXPENSES',
+  expenses
+})
+
+export const startSetExpenses=()=>{
+  return (dispatch)=>{
+    return database.ref('expenses').once('value').then((snapshot)=>{
+      const expenses =[]
+
+      snapshot.forEach((childSnapshot)=>{
+        expenses.push({
+          id:childSnapshot.key,
+          ...childSnapshot.val()
+        })
+      })
+      dispatch(setExpenses(expenses))
+    })
+  }
+}
